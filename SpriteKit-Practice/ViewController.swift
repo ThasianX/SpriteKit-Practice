0fb6367a19be9ae08e2f3451598967a3cb37d597
Kevin Li <kevinli29033@gmail.com>
2020-07-20 20:47:52 -0700

    Initial Commit

---
+++ SpriteKit-Practice/ViewController.swift
@@ -0,0 +1,14 @@
+// Kevin Li - 8:47 PM - 7/20/20
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
