9133c34a46f20a03d427d8163e423a07d5d2caba
Kevin Li <kevinli29033@gmail.com>
2020-07-21 17:09:49 -0700

    initial implementation of circular gravity

--- SpriteKit-Practice/ViewController.swift
+++ SpriteKit-Practice/ViewController.swift
@@ -41,6 +41,8 @@ class PlanetScene: SKScene {
         addChild(planet)
 
         showElements()
+        configurePermanentRotation()
+        configureRadialField()
     }
 
     private func showElements() {
@@ -50,7 +52,8 @@ class PlanetScene: SKScene {
             elementNode.fillColor = .random
 
             elementNode.physicsBody = SKPhysicsBody(circleOfRadius: circleRadius)
-            addChild(elementNode)
+            elementNode.physicsBody?.density = 100
+            planet.addChild(elementNode)
 
             elementNode.position = CGPoint(
                 x: Int.random(in: -(Int(size.width) / 2)..<(Int(size.width) / 2 - Int(circleRadius))),
@@ -58,18 +61,38 @@ class PlanetScene: SKScene {
         }
     }
 
+    private func configurePermanentRotation() {
+        let rotateAction = SKAction.rotate(byAngle: CGFloat(2 * Double.pi), duration: 40)
+        planet.run(SKAction.repeatForever(rotateAction))
+    }
+
+    private func configureRadialField() {
+        let strength = Float(max(size.width, size.height))
+        let radius = strength.squareRoot() * 100
+
+        let field = SKFieldNode.radialGravityField()
+        addChild(field)
+        field.region = SKRegion(radius: radius)
+        field.minimumRadius = radius
+        field.strength = 1
+    }
+
     override func update(_ currentTime: TimeInterval) {
-        planet.updateGravity(children: children)
+        planet.updateGravity()
     }
 
 }
+
+fileprivate let dt: CGFloat = 1.0/60.0 //Delta Time
+fileprivate let period: CGFloat = 10 //Number of seconds it takes to complete 1 orbit.
+fileprivate let orbitRadius = CGPoint(x: 25, y: 25) //Radius of orbit.
 
 class Planet: SKShapeNode {
 
     var gravityRange: CGFloat!
     var gravityMultiplier: CGFloat!
 
-    func updateGravity(children: [SKNode]) {
+    func updateGravity() {
         for child in children {
             let xDelta = position.x - child.position.x
             let yDelta = position.y - child.position.y
